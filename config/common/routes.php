59d50968f65c57de8c4a6854b9f001331a41faeb
Fluorouacil <artem.super.05@gmail.com>
2026-04-25 18:15:35 +0400

    Добавлены маршруты и обработчики HTTP-запросов в Yii 3

--- config/common/routes.php
+++ config/common/routes.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use App\Web;
 use Yiisoft\Router\Group;
 use Yiisoft\Router\Route;
+use App\Web\Api\EchoAction;
+use App\Web\Api\PingAction;
+use App\Web\HelloPage\Action as HelloPageAction;
 
 return [
     Group::create()
@@ -13,4 +16,7 @@ return [
                 ->action(Web\HomePage\Action::class)
                 ->name('home'),
         ),
+    Route::get('/hello')->action(HelloPageAction::class),
+    Route::get('/api/ping')->action(PingAction::class),
+    Route::post('/api/echo')->action(EchoAction::class),
 ];
